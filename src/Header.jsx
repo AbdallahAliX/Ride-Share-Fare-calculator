0f148073b6e1b9235b7431862de5bde84cca77bf
Abdallah Mohamed Ali <abdallahali0004@gmail.com>
2023-09-23 15:24:28 +0300

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img src="../public/logo3.png" className="logo" />
+            <h2 className="title">FareMate</h2>
+        </header>
+    )
+}
