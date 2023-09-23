b68053173953693c79aeb04fc3b43e7c3707dfaf
Abdallah Mohamed Ali <abdallahali0004@gmail.com>
2023-09-23 16:31:19 +0300

    more styling

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 export default function Calculator() {
     return(
-        <div className="container">
-            <div className="input-grid">
-                <input className="duration" type="number" placeholder="Enter Fare Duration in minuits" />
-                <input className="distance" type="number" placeholder="Enter Distance in miles" /> 
-                <button className="calculate-button js-calculate">Calculate</button>
+        <>
+            <div className="container">
+                <img className="calculator-logo" src="../public/logo4.png" /> 
+                <img className="calculator-logo" src="../public/car-logo.png" /> 
             </div>
-           
-        </div>
+            
+            <div className="form-container">
+                <div className="input-grid">
+                    <input className="duration" type="number" placeholder="Enter Fare Duration in minuits" />
+                    <input className="distance" type="number" placeholder="Enter Distance in miles" /> 
+                    <button className="calculate-button js-calculate">Calculate</button>
+                </div>
+            </div>
+        </>
     )
 }
